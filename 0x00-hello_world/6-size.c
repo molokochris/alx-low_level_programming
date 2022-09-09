5d37a75d68f28eb194661f6644dc07a7e22dc821
molokochris <molokochrisp742@gmail.com>
2022-09-08 19:48:03 -0700

    fixed for 34bit & 64bit machones

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	printf("Size of Char: %d byte(s)\n", sizeof(char));
+	printf("Size of Char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
