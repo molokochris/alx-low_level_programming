a49b3ed763aac656c31f32946fd38408977af344
molokochris <molokochrisp742@gmail.com>
2022-09-12 10:30:11 -0700

    fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int i = 0;
-	char alpha[26] = "abcdefghijklmnopqrstuvwxyz\n";
+	char alpha[28] = "abcdefghijklmnopqrstuvwxyz\n";
 		while (alpha[i] != '\0')
 		{
 			putchar(alpha[i]);
