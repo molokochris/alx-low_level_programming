8530ebd7a1e9e210ec8ab8153fd871104e43b961
molokochris <molokochrisp742@gmail.com>
2022-09-13 16:24:05 -0700

    second task first comm

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet prints alphabets
+ */
+void print_alphabet(void)
+{
+	int alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+return (0);
+}
