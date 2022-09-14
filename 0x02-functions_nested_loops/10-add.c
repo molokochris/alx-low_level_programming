8cc9ea33174a852233b9732df00d7691b2c596e0
molokochris <molokochrisp742@gmail.com>
2022-09-13 17:56:41 -0700

    all

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - find sum of two integers
+ * @a: integer input
+ * @b: integer input 2
+ * Return: sum of the two
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+return (sum);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: the number the count starts with
+ */
+void print_to_98(int n)
+{
+	int i = n;
+	if (n > 98)
+	{
+		while (i >= 98)
+		{
+			printf("%d, ", i);
+			--i;
+		}
+	}
+	else
+	{
+		while (i <=98)
+		{
+			printf("%d, ", i);
+			++i;
+		}
+	}
+}
