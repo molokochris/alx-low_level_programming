d29b409aa7232f624d58ab6830c6796f07b80c3a
molokochris <molokochrisp742@gmail.com>
2022-09-30 20:55:09 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - takes two int and return product
  * @argc: number of arguments
  * @argv: value of the arguments
- * Return:0
+ * Return:0 or 1
  */
 int main(int argc, char *argv[])
 {
-	int a = *argv[1] - '0', b = *argv[2] - '0';
-	int prod = b * a;
+	int prod = atoi(argv[1]) * atoi(argv[2]);
 
 	if (argc != 3)
+	{
 		printf("Error\n");
-	else
-		printf("%d\n", prod);
+		return (1);
+	}
+	printf("%d\n", prod);
 	return (0);
 }
