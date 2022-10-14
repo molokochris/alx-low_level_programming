fa8390a57f498b98a0196a8ce5cef0ec6da9b9d6
molokochris <molokochrisp742@gmail.com>
2022-10-14 02:20:08 -0700

    #7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * puts_half - takes a string and printf half of the string
+ * @str: the string being printed
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		i++;
+	}
+
+	i -= 1;
+
+	if (i % 2 == 0)
+	{
+		i = i / 2;
+		
+		while (*(str + i))
+		{
+			putchar(*(str + i));
+			i++;
+		}
+	}
+	else
+	{
+		i = (i - 1) / 2;
+
+		while (*(str + i))
+		{
+			putchar(*(str + i));
+			i++;
+		}
+	}
+	putchar('\n');
+}
