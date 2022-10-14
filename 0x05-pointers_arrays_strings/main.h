d5780c4e0f4f85e5d40228138c6a51fa712ec2e1
molokochris <molokochrisp742@gmail.com>
2022-10-14 01:08:36 -0700

    #5

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-include <stdio.h>
+#include <stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,5 +10,6 @@ void _puts(char *str);
 int _putchar(char c);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif /* MAIN_H */
