7d2582c758b625f4d5f218637a189f38fb8e8ece
molokochris <molokochrisp742@gmail.com>
2022-10-31 01:35:41 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,17 @@
  */
 void more_numbers(void)
 {
-	char i[20] = "01234567891011121314";
-	int z = 0;
-		while (z < 10)
+	int count = 0, i = 0;
+
+	while (count < 10)
+	{
+		while (i < 14)
 		{
-			int j = 0;
-				while (z < 10)
-				{
-					_putchar(i[j]);
-					j++;
-				}
-			z++;
-			_putchar('\n');
+			_putchar(i + '0');
+			i++;
 		}
+		_putchar('\n');
+		i = 0;
+		count++;
+	}
 }
