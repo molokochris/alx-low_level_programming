6416e61494efb7aa43cc77e4bf94912700b641c6
molokochris <molokochrisp742@gmail.com>
2023-11-28 05:19:36 +0200

    dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * Definition of the dog structure
+ * The structure represents information about the dog,
+ * which is the name, age, and owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
